c98aafef2ffefe6973e43c61998d3a2c5840bee3
DeveloperHan <lefthanddeveloper@gmail.com>
2022-09-01 01:14:44 +0900

    add unity chan shader color organs

---
+++ Assets/DancingOrgans/Scripts/CenterFinder.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CenterFinder : MonoBehaviour
+{
+   
+   public void FindCenterPosition()
+	{
+		Vector3 center = GetComponent<Renderer>().bounds.center;
+		Debug.Log(center);
+	}
+}
+
+[CustomEditor(typeof(CenterFinder))]
+public class CenterFinderEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		var script = (CenterFinder)target;
+
+		if(GUILayout.Button("Find Center"))
+		{
+			script.FindCenterPosition();
+		}
+	}
+}
